945acd08bc0074e89d90a8bc679a4bb54372cb46
semyers189 <semyers@fullsail.edu>
2021-04-07 23:15:46 -0400

    added mongoose and setup files

--- bot.js
+++ bot.js
@@ -13,11 +13,30 @@ app.listen(port, () => {
 });
 
 const Discord = require('discord.js');
+const connectDB = require('./config/db');
 
 const client = new Discord.Client();
+const prefix = ']';
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
+connectDB();
+
 client.on('ready', () => {
   console.log('Bot is ready!');
+});
+
+client.on('message', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) {
+    return;
+  }
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+    default:
+      message.channel.send(`${command} command does not exist!`);
+      return;
+  }
 });
