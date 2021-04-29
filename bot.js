500a385c342199b8bbb2b95b7d783fa595370629
semyers189 <semyers@fullsail.edu>
2021-04-28 20:44:46 -0400

    added test route

--- bot.js
+++ bot.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+app.get('/json', (req, res) => {
+  res.json({ message: 'hi' });
+});
+
 app.listen(port, () => {
   console.log(`Birbit listening on port ${port}`);
 });
